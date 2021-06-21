f11691122e0f8fba47ebaddae7be36890529c2ac
elyor doniyorov <doniyorovelyor@gmail.com>
2021-06-22 01:12:26 +0500

    install avatar uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 15 }
   validates :fullname, presence: true
-  mount_uploader :photo, ImageUploader
-  mount_uploader :background_image, ImageUploader
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 

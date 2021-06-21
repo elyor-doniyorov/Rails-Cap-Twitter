b6f9e974d2d5f9c91a1f8f3c8b45b4f277605ad9
elyor doniyorov <doniyorovelyor@gmail.com>
2021-06-22 00:43:58 +0500

    create likes and followings controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+  
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:fullname, :username, :email, :password, :password_confirmation, :photo, :background_image)
+      end
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:fullname, :username, :email, :password, :password_confirmation, :photo, :background_image, :current_password)
+      end
+    end
 end

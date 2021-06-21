09ecf8de830ab9c2c7294d1e2d00f02e90f5e4b2
elyor doniyorov <doniyorovelyor@gmail.com>
2021-06-22 02:21:10 +0500

    fix more stylint errors

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'twitter-redesign'
+    config.api_key = '594216161619791'
+    config.api_secret = 'PqDQO9LIH7NTae5xG1CN-aGwAf4'
+    config.secure = true
+    config.cdn_subdomain = true
+end

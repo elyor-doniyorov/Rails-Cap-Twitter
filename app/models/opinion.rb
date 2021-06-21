e2ad2c507c0540b3164650da3cdbd1edf6cabe3e
elyor doniyorov <doniyorovelyor@gmail.com>
2021-06-22 00:59:45 +0500

    add associations to opinion model

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,3 +1,7 @@
 class Opinion < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  validates :text, presence: true, length: { maximum: 260 }
+  has_many :likes, dependent: :destroy
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+  scope :newsfeed_opinions, -> { includes(:author).ordered_by_most_recent }
 end

008ac7727cd3e6e12a77b4c7b388a2cf91096679
elyor doniyorov <doniyorovelyor@gmail.com>
2021-06-22 01:30:38 +0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  root 'opinions#index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :index] do
+    member do
+      get '/follow_user/:id', to: 'users#follow_user', as: :follow
+    end
+  end
+      resources :followings do
+member do
+        get '/destroy_user/:id', to: 'followings#destroy_user', as: :destroy
+end
+end
+
+  resources :opinions, only: [:index, :create, :new, :destroy] do
+    resources :likes, only: [:create, :destroy]
+  end
+
 end
